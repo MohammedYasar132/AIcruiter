c183479814f5443445d017eebd4899640e19a498
MohammedYasar132 <mohammedyasar132@gmail.com>
2026-03-04 23:18:24 +0530

    Update CORS origin to production URL

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ import paymentRouter from "./routes/payment.route.js"
 
 const app = express()
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://aicruiter-4s7x.onrender.com",
     credentials:true
 }))
 
