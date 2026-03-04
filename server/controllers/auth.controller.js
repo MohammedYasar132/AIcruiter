da4ef4e057bbbca0844f6c1f351d3391a86013e5
MohammedYasar132 <mohammedyasar132@gmail.com>
2026-03-05 01:23:35 +0530

    Include path option in cookie configuration
    Added 'path' option to cookie settings.

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -23,10 +23,11 @@ export const googleAuth = async (req,res) => {
     const token = await genToken(user._id);
 
     res.cookie("token", token, {
-      httpOnly: true,
-      secure: true,
-      sameSite: "none",
-      maxAge: 7 * 24 * 60 * 60 * 1000
+        httpOnly: true,
+        secure: true,
+        sameSite: "none",
+        path: "/",
+        maxAge: 7 * 24 * 60 * 60 * 1000
     });
 
     return res.status(200).json(user);
